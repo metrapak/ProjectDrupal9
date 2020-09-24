3ff7791af6bae3b6bfd9f8cf88d466a69a41dc82
artem_pokachajlo <pokachajloartem@gmail.com>
2020-09-24 14:17:52 +0300

    reformat code

--- web/modules/custom/test_block/src/Plugin/Block/DefaultBlock.php
+++ web/modules/custom/test_block/src/Plugin/Block/DefaultBlock.php
@@ -4,14 +4,14 @@ namespace Drupal\test_block\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 
-class DefaultBlock extends BlockBase
-{
-    public function build()
-    {
-        $renderable = [
-            '#theme' => 'default_block',
-            '#hello_var' => 'Hello World',
-        ];
-        return $renderable;
-    }
+class DefaultBlock extends BlockBase {
+
+  public function build() {
+    $renderable = [
+      '#theme' => 'default_block',
+      '#hello_var' => 'Hello World',
+    ];
+    return $renderable;
+  }
+
 }
