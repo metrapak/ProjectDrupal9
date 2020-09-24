04f86bc4a118b04972187c10582fd4cefdd81bd7
artem_pokachajlo <pokachajloartem@gmail.com>
2020-09-24 14:12:06 +0300

    reformat code

--- web/modules/custom/a_p_task92/src/Plugin/Block/Task92_Plugin1.php
+++ web/modules/custom/a_p_task92/src/Plugin/Block/Task92_Plugin1.php
@@ -1,17 +1,18 @@
 <?php
+
 namespace Drupal\a_p_task92\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 
-class Task92_Plugin1 extends BlockBase
-{
-   public function build()
-    {
-       $renderable = [
-            '#theme' => 'task92_plugin1',
-            '#message' => 'Hello World!',
-            ];
+class Task92_Plugin1 extends BlockBase {
 
-       return $renderable;
-   }
+  public function build() {
+    $renderable = [
+      '#theme' => 'task92_plugin1',
+      '#message' => 'Hello World!',
+    ];
+
+    return $renderable;
+  }
+
 }
--- web/modules/custom/a_p_task92/src/Plugin/Block/Task92_Plugin2.php
+++ web/modules/custom/a_p_task92/src/Plugin/Block/Task92_Plugin2.php
@@ -1,26 +1,26 @@
 <?php
+
 namespace Drupal\a_p_task92\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 
-class Task92_Plugin2 extends BlockBase
-{
+class Task92_Plugin2 extends BlockBase {
 
-     public function build()
-     {
-        $header = ['id','title', 'created'];
-        $nodes = $query = \Drupal::database()->select('node_field_data', 'nfd')
-        ->fields('nfd', ['nid', 'title','created'])->execute()->fetchAll();
-        $rows = array();
+  public function build() {
+    $header = ['id', 'title', 'created'];
+    $nodes = $query = \Drupal::database()->select('node_field_data', 'nfd')
+      ->fields('nfd', ['nid', 'title', 'created'])->execute()->fetchAll();
+    $rows = [];
 
-        foreach ($nodes as $node) {
-            $rows[] = array($node->nid, $node->title,$node->created);
-        }
+    foreach ($nodes as $node) {
+      $rows[] = [$node->nid, $node->title, $node->created];
+    }
 
-        return [
-            '#type' => 'table',
-            '#header' => $header,
-            '#rows' => $rows,
-             ];
-      }
+    return [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
   }
+
+}
