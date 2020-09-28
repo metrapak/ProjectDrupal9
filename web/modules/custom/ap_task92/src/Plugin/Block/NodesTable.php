ee7360e69d209d271166df59ab0499b37f20e2e8
artem_pokachajlo <pokachajloartem@gmail.com>
2020-09-28 11:57:06 +0300

    rename plugins

---
+++ web/modules/custom/ap_task92/src/Plugin/Block/NodesTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\ap_task92\Plugin\Block;
+
+
+use Drupal;
+
+use Drupal\Core\Block\BlockBase;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * Provides a 'NodesTable' block.
+ *
+ * @Block(
+ *  id = "nodes_table",
+ *  admin_label = @Translation("Nodes table"),
+ * )
+ */
+class NodesTable extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $header = ['id', 'title', 'created'];
+    $nids = Drupal::entityQuery('node')->execute();
+    $nodes = Node::loadMultiple($nids);
+    $rows = [];
+    foreach ($nodes as $node) {
+      $rows[] = [$node->nid->value, $node->title->value, $node->created->value];
+    }
+
+    return [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+
+  }
+
+}
